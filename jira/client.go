875343f7fdbeee048b558d0b4010da0b009be868
Even <songyanping@fulan.com.cn>
2024-05-06 11:56:15 +0800

    update jira client

--- jira/client.go
+++ jira/client.go
@@ -3,6 +3,7 @@ package jira
 import (
 	"fmt"
 	"github.com/andygrunwald/go-jira"
+	"time"
 )
 
 func NewJiraConfig(url, username, password string) JiraConfig {
@@ -49,4 +50,52 @@ func (c *Client) Get(issueId string) (summary string) {
 		fmt.Println(summary)
 	}
 	return summary
+}
+
+func (c *Client) Create(summary, description string) (err error) {
+	var comps []*jira.Component
+	comp := jira.Component{
+		Name: "运维",
+	}
+	comps = append(comps, &comp)
+	issue := jira.Issue{
+		Fields: &jira.IssueFields{
+			//Assignee: &go_jira.User{
+			//	Name:  config.Config.Jira.Assignee,
+			//},
+			//Reporter: &go_jira.User{
+			//	Name:  config.Config.Jira.Reporter,
+			//},
+			Description: description,
+			Type: jira.IssueType{
+				Name: "type",
+			},
+			Project: jira.Project{
+				Key: "project",
+			},
+			Summary:    summary,
+			Components: comps,
+			Priority: &jira.Priority{
+				Name: "priority",
+			},
+		},
+	}
+	iss, resp, err := c.client.Issue.Create(&issue)
+	fmt.Println(resp.Body)
+	fmt.Println(iss.ID)
+	return err
+}
+
+type JiraIssue struct {
+	IssueId       string    `json:"issue_id" bson:"issue_id"`
+	Assignee      string    `json:"assignee" bson:"assignee"`
+	Reporter      string    `json:"reporter" bson:"reporter"`
+	Summary       string    `json:"summary" bson:"summary"`
+	Description   string    `json:"description" bson:"description"`
+	ComponentName string    `json:"component_name" bson:"component_name"`
+	Project       string    `json:"project" bson:"project"`
+	Issuetype     string    `json:"issue_type" bson:"issue_type"`
+	Priority      string    `json:"priority" bson:"priority"`
+	Created       time.Time `json:"created" bson:"created"`
+	Updated       time.Time `json:"updated" bson:"updated"`
 }
