0cf186a5a18035961fd3091ec373dbc78903ed38
Even <songyanping@fulan.com.cn>
2024-05-17 15:52:12 +0800

    update es

--- elasticsearch/elasticsearch.go
+++ elasticsearch/elasticsearch.go
@@ -3,9 +3,11 @@ package elasticsearch
 import (
 	"fmt"
 	es8 "github.com/elastic/go-elasticsearch/v8"
+	"strings"
+	"time"
 )
 
-func NewEsConfig(host, port, username, password, protocol string) EsConfig {
+func NewEsConfig(host string, port int, username, password, protocol string) EsConfig {
 	return EsConfig{
 		Host:     host,
 		Port:     port,
@@ -17,13 +19,13 @@ func NewEsConfig(host, port, username, password, protocol string) EsConfig {
 
 type EsConfig struct {
 	Host     string
-	Port     string
+	Port     int
 	Username string
 	Password string
 	Protocol string
 }
 
-func NewSearchClient(config EsConfig) (*Client, error) {
+func NewSearchClient(config EsConfig) (*EsClient, error) {
 	cfg := es8.Config{
 		Addresses: []string{
 			fmt.Sprintf("%s://%s:%d", config.Protocol, config.Host, config.Port),
@@ -35,11 +37,33 @@ func NewSearchClient(config EsConfig) (*Client, error) {
 	if err != nil {
 		return nil, fmt.Errorf("Error creating the client: %s", err)
 	}
-	return &Client{
+
+	return &EsClient{
 		client: client,
 	}, nil
 }
 
-type Client struct {
+type EsClient struct {
 	client *es8.Client
+}
+
+func (es *EsClient) SearchContent(index, query string) (result string, err error) {
+	res, err := es.client.Search(
+		es.client.Search.WithIndex(index),
+		es.client.Search.WithBody(strings.NewReader(query)),
+		es.client.Search.WithPretty(),
+		es.client.Search.WithTimeout(60*time.Second),
+	)
+	if err != nil {
+		fmt.Errorf("Error connect search: %s", err)
+		return
+	}
+	defer res.Body.Close()
+
+	if res.IsError() {
+		fmt.Errorf("[%s] Error getting response: %s", res.Status(), res.String())
+		return
+	}
+	result = res.String()
+	return
 }
---
+++ elasticsearch/elasticsearch_test.go
@@ -0,0 +1,101 @@
+package elasticsearch
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+	"testing"
+	"time"
+)
+
+type Event struct {
+	Time        time.Time              `json:"time"`
+	StartsAt    string                 `json:"starts_at"`
+	EndsAt      string                 `json:"ends_at"`
+	Duration    int64                  `json:"duration"` //持续时间
+	EventId     string                 `json:"event_id"`
+	Severity    string                 `json:"severity"`
+	Status      string                 `json:"status"`
+	Service     string                 `json:"service" mapping:"type:keyword"`
+	Title       string                 `json:"title" mapping:"type:keyword"`
+	Env         string                 `json:"env"`
+	AppId       string                 `json:"app_id" mapping:"type:keyword"`
+	DataSource  string                 `json:"data_source" mapping:"type:keyword"`
+	Path        string                 `json:"path" mapping:"type:keyword"`
+	Reason      string                 `json:"reason" yaml:"reason" mapping:"type:keyword"`
+	Description string                 `json:"description" yaml:"description" mapping:"type:keyword"`
+	RuleType    string                 `json:"rule_type" yaml:"rule_type" mapping:"type:keyword"`
+	InstanceId  string                 `json:"instance_id" yaml:"instance_id" mapping:"type:keyword"`
+	Details     map[string]interface{} `json:"details" yaml:"details" mapping:"type:object"`
+	Labels      map[string]string      `json:"labels" yaml:"labels" mapping:"type:object"`
+}
+
+type SearchResultEvent struct {
+	Took      int  `json:"took"`
+	Timed_out bool `json:"timed_out"`
+	Shards    struct {
+		Total      int `json:"total"`
+		Successful int `json:"successful"`
+		Skipped    int `json:"skipped"`
+		Failed     int `json:"failed"`
+	} `json:"_shards"`
+	Hits struct {
+		Total struct {
+			Value    int    `json:"value"`
+			Relation string `json:"relation"`
+		} `json:"total"`
+		Max_score float64 `json:"max_score"`
+		Hits      []struct {
+			Index  string  `json:"_index"`
+			Type   string  `json:"_type"`
+			Id     string  `json:"_id"`
+			Score  float64 `json:"_score"`
+			Source Event   `json:"_source"`
+			Sort   []int   `json:"sort"`
+		} `json:"hits"`
+	} `json:"hits"`
+	Summary string `json:"_summary"`
+}
+
+func queryEvents(client *EsClient, index, query string) (result []Event, err error) {
+	//var qry = `{"query": {"bool": {"filter": [{"term":{"summary_date": "SUMMARYDATE"}}]}},"sort": [{"time": "desc"}]}`
+	//qry = strings.Replace(qry, "SUMMARYDATE", query, -1)
+	//fmt.Println("Elasticsearch eventSummary query: %s", qry)
+
+	res, err := client.SearchContent(index, query)
+	if err != nil {
+		fmt.Errorf("Error getting elasticsearch response: %s", err.Error())
+		return result, err
+	}
+
+	var searchResultEvent SearchResultEvent
+	err = json.Unmarshal([]byte(strings.Replace(res, "[200 OK]", "", -1)), &searchResultEvent)
+	if err != nil {
+		fmt.Sprintf("Error json unmarshal: %s", err.Error())
+		return result, err
+	}
+	for _, hit := range searchResultEvent.Hits.Hits {
+		result = append(result, hit.Source)
+	}
+
+	return result, nil
+}
+
+func TestElasticsearchClient(t *testing.T) {
+	Conf := NewEsConfig("10.158.215.42", 9200, "", "", "http")
+	client, err := NewSearchClient(Conf)
+	if err != nil {
+		t.Error(err)
+	}
+
+	var qry = `{"query": {"bool": {"filter": [{"term":{"env": "pd"}}]}},"size":10,"sort": [{"time": "desc"}]}`
+	events, err := queryEvents(client, "events", qry)
+	if err != nil {
+		t.Error(err)
+	}
+
+	for _, i := range events {
+		fmt.Println(i.Service)
+	}
+
+}
